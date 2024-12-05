ea47b1ba433ea0212c55d4e70ac07e337ba77696
Sebastian Diaz Jimenez <129464465+SebastianDiazJ@users.noreply.github.com>
2024-12-05 11:24:11 -0500

    footer

--- Ecomerce soval/src/componentes/Footer/Footer.js
+++ Ecomerce soval/src/componentes/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-// components/Footer.js
+import { Link } from 'react-router-dom'; // Si necesitas usar Link para navegación interna
 
 const Footer = () => {
     return (
         <footer className="footer">
             <p>&copy; 2024 SOVAL Todos los derechos reservados.</p>
+            <p>&copy; Desarrollado por <a href="https://sebastiandiazdeveloper.rf.gd/home/vol14_2/infinityfree.com/if0_37393918/?i=1" target="_blank" rel="noopener noreferrer">Sebastian Diaz</a></p>
         </footer>
     );
 };
